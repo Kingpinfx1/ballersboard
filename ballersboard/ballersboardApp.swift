f95b6c33c61dfa6c94fd7b92fdae8b5f527a881b
kingpin <kingpinfx1@gmail.com>
2025-06-13 22:48:30 +0100

    Initial Commit

---
+++ ballersboard/ballersboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ballersboardApp.swift
+//  ballersboard
+//
+//  Created by kingpin on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ballersboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
